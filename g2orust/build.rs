7fcf2090661ffc95d9d0536dfd744defbce5e85c
pranayspeed <pranayspeed@gmail.com>
2022-10-19 11:15:30 -0400

    build fix v1

--- g2orust/build.rs
+++ g2orust/build.rs
@@ -5,7 +5,7 @@ fn main() {
 
     // Pranay: Can use the following flag to set cmake::Config .cxxflag to include directory path for resolving the lib.rs .h file search path issue
     // But Fix the CMakeList.txt instead to include the path itself
-    let _manifest_dir = concat!("-I",concat!(env!("CARGO_MANIFEST_DIR"),"/g2o/g2o/rust/"));
+    let manifest_dir = concat!("-I",concat!(env!("CARGO_MANIFEST_DIR"),"/g2o/g2o/rust/"));
 
     let _dst = Config::new("g2o")
                         .cxxflag(manifest_dir)
